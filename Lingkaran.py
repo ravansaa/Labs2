9ed0912734945ad42eef4c558b7eac4f160b33db
ravansaa <ravansa99@gmail.com>
2021-10-30 22:35:57 +0700

    Menambahkan File

---
+++ Lingkaran.py
@@ -0,0 +1,8 @@
+import math
+r = float(input("Masukan jari-jari :"))
+
+l = math.pi*(r*r)
+k = 2*math.pi*r
+
+print("luas Lingkaran \t= ",format(l,'.2f'))
+print("Keliling Lingkaran \t= ",format(k,'.2f'))
